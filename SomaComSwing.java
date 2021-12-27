09b19aaf7aa4ee3be2020f1fda2c9c3b9482d7f9
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-27 02:08:12 -0300

    Update SomaComSwing.java

--- SomaComSwing.java
+++ SomaComSwing.java
@@ -1,6 +1,6 @@
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         // TODO add your handling code here:
-        int valor1 = Integer.parseInt(n1.getText());
-        int valor2 = Integer.parseInt(n2.getText());
-        String soma = Integer.toString( valor1 + valor2 );
-        lbl.setText(soma);
+        int valor1 = Integer.parseInt(n1.getText());//pegando valores do campo de texto e convertendo para inteiro
+        int valor2 = Integer.parseInt(n2.getText());//pegando valores do campo de texto e convertendo para inteiro
+        String resultado = Integer.toString( valor1 + valor2 );//somando valores inteiros e convertendo para string
+        lbl.setText(resultado);//configurando texto para o resultado
