8232afb058d0dd8838fc324d0f5d6b2bcf23c9fc
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-27 01:59:44 -0300

    Create ConvertendoTipos.java

---
+++ ConvertendoTipos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testestipos;
+
+/**
+ *
+ * @author renan
+ */
+public class TestesTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int idade1 = 30;
+        String valor1 = Integer.toString(idade1);
+        //convertendo inteiro para string
+        System.out.println(valor1);
+        String valor2 = "40";
+        int idade2 = Integer.parseInt(valor2);
+        //convertendo string para inteiro
+        System.out.println(idade2);
+        
+    }
+    
+}
