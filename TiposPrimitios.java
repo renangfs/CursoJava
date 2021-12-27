125e201014e2f80795946ea68fab23a14b921a91
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-26 22:13:19 -0300

    Create TiposPrimitios.java

---
+++ TiposPrimitios.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author renan
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome = "Renan";//guardando "Renan" na variavel nome
+        float numero1 = 1.5f;//guardando "1.5" na variavel numero1
+        int numero2 = 3; //guardando "3" na variavel numero2
+        System.out.printf("O resultado da variavel 'nome' é: %s \n", nome);
+        //mostrando resultado da string
+        System.out.printf("O resultado da variavel 'numero1' é: %.2f\n", numero1);
+        //mostrando resultado do float
+        System.out.printf("O resultado da variavel 'numero2 é: %s \n", numero2);
+        //mostrando resultado do inteiro   
+    }
+    
+}
