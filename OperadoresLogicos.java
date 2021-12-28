a5acf281bba36f6a8248de449ee7f84337e74789
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-28 14:20:56 -0300

    Create OperadoresLogicos.java

---
+++ OperadoresLogicos.java
@@ -0,0 +1,9 @@
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x,y,z;
+        x=4;
+        y=7;
+        z=12;
+        boolean r;
+        r = (x<y && y<z)?true:false;
+        System.out.println(r);
