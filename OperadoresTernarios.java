05a6d94fd0ddf2d909725bcf63a25db7e7f25d65
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-28 14:10:52 -0300

    Create OperadoresTernarios.java

---
+++ OperadoresTernarios.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1,n2,r;
+        n1 = 4;
+        n2 = 8;
+        r = (n1>n2)?0:1;
+        System.out.println(r);
