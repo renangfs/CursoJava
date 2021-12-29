2b296d03d65713a04a478283c3115307e2ccccb7
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-29 05:55:46 -0300

    Create Contador.java

---
+++ Contador.java
@@ -0,0 +1,15 @@
+public class Contador01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 1;
+        while(cc<5){
+            System.out.println(cc);
+            cc++;
+        }
+    }
+    
+}
