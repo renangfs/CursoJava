a225bee6624818e7402784e19f879aeef2d66465
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-31 04:57:33 -0300

    Create Vetor02.java

---
+++ Vetor02.java
@@ -0,0 +1,16 @@
+public class Vetor02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String mes[] ={"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        int dias[] ={31,28,31,30,31,31,30,31,30,31,30,31};
+        for(int c=0; c<mes.length; c++){
+            System.out.println("O mes de " +mes[c]+" tem "+dias[c]+" dias");
+            
+        }
+    }
+                                                               
+    
