36188f21ba705fb3df4482901a24cf4c70dfbf48
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-12-31 04:57:03 -0300

    Create Vetor01.java

---
+++ Vetor01.java
@@ -0,0 +1,17 @@
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {2,4,5,23};
+        
+        for(int c = 0 ; c < n.length-1  ; c++){
+            System.out.print(n[c]+"-");
+        }
+        
+        
+          
+    }
+}
